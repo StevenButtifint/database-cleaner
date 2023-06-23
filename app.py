4be8474a55b7f54fdbd327353cedc111cf8d5d37
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-06-23 14:04:41 +0100

    added tool page setup

--- app.py
+++ app.py
@@ -24,6 +24,14 @@ class Window(QtWidgets.QMainWindow):
         btn_exit = self.findChild(QPushButton, 'btn_exit')
         btn_exit.clicked.connect(lambda: self.close())
 
+    def _setup_tools_page(self):
+        btn_tools_back = self.findChild(QPushButton, 'btn_tools_back')
+        btn_tools_back.clicked.connect(lambda: self.main_page_stack.setCurrentWidget(self.findChild(QWidget, 'home_page')))
+        btn_analysis_page = self.findChild(QPushButton, 'btn_analysis_page')
+        btn_analysis_page.clicked.connect(lambda: self.main_page_stack.setCurrentWidget(self.findChild(QWidget, 'analysis_results_page')))
+        btn_cleaning_page = self.findChild(QPushButton, 'btn_cleaning_page')
+        btn_cleaning_page.clicked.connect(lambda: self.main_page_stack.setCurrentWidget(self.findChild(QWidget, 'cleaning_page')))
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
