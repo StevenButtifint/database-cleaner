c1f7e8b57a9ac6a480aa46f7e47ba922d07da2cd
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-08-17 15:22:49 +0100

    updated home page setup

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ class Window(QtWidgets.QMainWindow):
         uic.loadUi(INTERFACE_DIR, self)
 
         self.main_page_stack = self.findChild(QStackedWidget, 'main_page_stack')
+        self.database_dir = ""
+        self.database_name = ""
+        self.output_dir = ""
 
         self.setup()
         self.show()
@@ -25,9 +28,11 @@ class Window(QtWidgets.QMainWindow):
 
     def _setup_home_page(self):
         btn_select_database = self.findChild(QPushButton, 'btn_select_database')
-        btn_select_database.clicked.connect(lambda: self._select_database())
+        btn_select_database.clicked.connect(lambda: self.select_database())
+        btn_select_output = self.findChild(QPushButton, 'btn_select_output')
+        btn_select_output.clicked.connect(lambda: self.set_output_dir())
         btn_home_continue = self.findChild(QPushButton, 'btn_home_continue')
-        btn_home_continue.clicked.connect(lambda: self.main_page_stack.setCurrentWidget(self.findChild(QWidget, 'tools_page')))
+        btn_home_continue.clicked.connect(lambda: self.switch_operations_page())
         btn_exit = self.findChild(QPushButton, 'btn_exit')
         btn_exit.clicked.connect(lambda: self.close())
 
