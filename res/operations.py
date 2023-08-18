040c2113fe9f32162a53c29f2d580ced9c209531
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-08-18 14:36:35 +0100

    added null over time function

--- res/operations.py
+++ res/operations.py
@@ -16,3 +16,17 @@ def get_null_count_percentage(dataset):
     return round((null_count / cell_count) * 100, 2)
 
 
+def get_null_count_over_time(dataset):
+    row_nulls_count = dataset.isnull().sum(axis=1).tolist()
+    total_count = len(row_nulls_count)
+    interval_size = total_count // 20
+    tenth_sums = []
+    current_position = 0
+    for _ in range(20):
+        end_position = current_position + interval_size
+        tenth_sum = sum(row_nulls_count[current_position:end_position])
+        tenth_sums.append(tenth_sum)
+        current_position = end_position
+    return tenth_sums
+
+
