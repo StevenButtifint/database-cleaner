4aa5b78dff2d67bd24b9192667e037933cdb5545
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-08-22 11:50:43 +0100

    added set database method

--- res/analysis.py
+++ res/analysis.py
@@ -3,9 +3,12 @@ from res.completeness import Completeness
 
 
 class Analysis:
-    def __init__(self, database):
-        self.database = database
+    def __init__(self):
+        self.database = None
         self.completeness_stats = Completeness(self.database)
+
+    def set_database(self, database):
+        self.database = database
 
     def calculate_completeness_stats(self):
         self.completeness_stats.set_database(self.database)
