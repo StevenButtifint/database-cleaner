2311742f3777ee2da826534f25818f595aab13a4
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-08-23 12:16:47 +0100

    added attribute methods

--- res/analysis.py
+++ res/analysis.py
@@ -5,10 +5,17 @@ from res.completeness import Completeness
 class Analysis:
     def __init__(self):
         self.database = None
+        self.attributes = None
         self.completeness_stats = Completeness(self.database)
 
     def set_database(self, database):
         self.database = database
+
+    def set_attributes(self, database):
+        self.attributes = get_csv_column_names(database)
+
+    def get_attributes(self):
+        return self.attributes
 
     def calculate_completeness_stats(self):
         self.completeness_stats.set_database(self.database)
