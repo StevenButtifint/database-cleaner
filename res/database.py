45c84e1daefa3928bfeb6d8b0c4961b3770b6175
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-08-24 12:50:16 +0100

    added database setters

--- res/database.py
+++ res/database.py
@@ -9,3 +9,19 @@ class Database:
         self.current_directory = None
         self.output_directory = None
 
+
+    def set_current_directory(self, directory):
+        self.current_directory = directory
+
+    def set_table(self, directory):
+        self.table = get_csv_data(directory)
+
+    def set_name(self, directory):
+        self.name = directory.split("/")[-1]
+
+    def set_attributes(self):
+        self.attributes = get_csv_column_names(self.table)
+
+    def set_output_directory(self):
+        self.output_directory = select_file()
+
