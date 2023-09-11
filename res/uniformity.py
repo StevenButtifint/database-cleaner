6c20be130facdec782422a51ad97c29255cf2884
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-09-11 21:03:40 +0100

    added setter methods

--- res/uniformity.py
+++ res/uniformity.py
@@ -8,6 +8,22 @@ class Uniformity:
         self.unique_percentage = 0
         self.unique_count = 0
 
+    def set_database_column(self, database_column):
+        self.database_column = database_column
+
+    def set_column_length(self, column_length):
+        self.column_length = column_length
+
+    def set_database(self, database_column):
+        self.set_database_column(database_column)
+        self.set_column_length(len(database_column))
+
+    def set_unique_percentage(self, unique_percentage):
+        self.unique_percentage = round(unique_percentage, 2)
+
+    def set_unique_count(self, unique_count):
+        self.unique_count = unique_count
+
     def get_column_length(self):
         return self.column_length
 
