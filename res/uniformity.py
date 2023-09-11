a8e164a3523671f5f02d24d75ffdcbef27ace40c
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-09-11 21:03:14 +0100

    added get methods

--- res/uniformity.py
+++ res/uniformity.py
@@ -8,3 +8,15 @@ class Uniformity:
         self.unique_percentage = 0
         self.unique_count = 0
 
+    def get_column_length(self):
+        return self.column_length
+
+    def get_database_column(self):
+        return self.database_column
+
+    def get_unique_count(self):
+        return self.unique_count
+
+    def get_unique_percentage_string(self):
+        return str(self.unique_percentage) + "%"
+
