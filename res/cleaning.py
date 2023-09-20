505f2be6f5c69406d2d487c2e074f08b44465246
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-09-20 11:05:42 +0100

    added active functions attributes

--- res/cleaning.py
+++ res/cleaning.py
@@ -8,3 +8,9 @@ class Cleaning:
     def __init__(self, database):
         self.database = database
         self.clean_database = None
+        self.remove_empty_attributes = False
+        self.remove_empty_records = False
+        self.remove_duplicate_records = False
+        self.remove_numerical_outliers = False
+        self.remove_date_outliers = False
+        self.remove_invalid_syntax = False
